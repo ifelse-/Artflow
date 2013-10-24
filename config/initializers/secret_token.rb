d7356d9cb9f5112f203d9e823a403ee90eeeb676
ifelse- <marvin@giveme5ive.com>
2013-10-23 22:28:39 -0400

    artflow demo ch1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Artflow::Application.config.secret_key_base = '9471f6e000dc46421cfd9cfdc09e564d5d25705581f3154c580f80b7496324a6506a5fde1d08e628aaa42d51a15aa39087ecbc70e140d1a6f8d1c18fbe0972d9'
